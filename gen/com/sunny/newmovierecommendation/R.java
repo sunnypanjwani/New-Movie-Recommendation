78cb7d1f2ce6db1fdab517daed47700e15314712
unknown <Sunny Panjwani@.(none)>
2011-09-05 19:07:44 -0700

    with list activity

--- gen/com/sunny/newmovierecommendation/R.java
+++ gen/com/sunny/newmovierecommendation/R.java
@@ -8,6 +8,9 @@
 package com.sunny.newmovierecommendation;
 
 public final class R {
+    public static final class array {
+        public static final int main_buttons=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,16 +18,19 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int ListView01=0x7f050003;
-        public static final int button=0x7f050000;
-        public static final int newMovieButton=0x7f050001;
-        public static final int upcomingMovieButton=0x7f050002;
+        public static final int button=0x7f060001;
+        public static final int closeButton=0x7f060004;
+        public static final int list_view=0x7f060000;
+        public static final int newMovieButton=0x7f060002;
+        public static final int upcomingMovieButton=0x7f060003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_view=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int close=0x7f040004;
         public static final int newMovies=0x7f040002;
         public static final int upcomingMovies=0x7f040003;
         public static final int whichMovie=0x7f040001;
--- src/com/sunny/newmovierecommendation/newMovieRecommendationActivity.java
+++ src/com/sunny/newmovierecommendation/newMovieRecommendationActivity.java
@@ -1,96 +1,38 @@
 package com.sunny.newmovierecommendation;
 
 import java.io.DataInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ViewSwitcher;
 
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
 
-public class newMovieRecommendationActivity extends Activity {
+public class newMovieRecommendationActivity extends ListActivity {
 
     Facebook facebook = new Facebook("190033754396514");
-    //private static String newMovieFilePath = "https://docs.google.com/document/d/1H73vqvvYoi9PDRNTtPp6x-l3IbZHrX6V1yCyNbMe4lc/edit?hl=en_US";
-    //private static String upcomingMovieFilePath = "https://docs.google.com/document/d/1ekJRicKyS64cpj0rt2Or5_tBjkNb2DyPUdfC9TKEIQE/edit?hl=en_US";
-    private static String newMovieFilePath = "http://sunnypanjwani.com/newmovies.txt";
-    private static String upcomingMovieFilePath = "http://sunnypanjwani.com/upcomingmovies.txt"; 
-    static URL url1, url2;
-    static private URLConnection urlConn1, urlConn2; 
+    
     static DataInputStream  dis1, dis2;
     Button button1, button2;
-    private ListView lv1;
-    static String[] stringArray1 = new String[20];
-    static String[] stringArray2 = new String[20];
-   
     
-    static{
-    	try {
-    		  String s = null; 
-    		  
-    		  url1 = new URL(newMovieFilePath);
-			  urlConn1 = url1.openConnection(); 
-			  urlConn1.setDoInput(true); 
-			  urlConn1.setUseCaches(false);
-			  dis1 = new DataInputStream(urlConn1.getInputStream()); 
-			  
-			  int i=0; 
-		      while ((s = dis1.readLine()) != null){
-				   stringArray1[i] = s;
-				   i++;
-			  }
-			    
-			  url2 = new URL(upcomingMovieFilePath);
-			  urlConn2 = url2.openConnection(); 
-			  urlConn2.setDoInput(true); 
-			  urlConn2.setUseCaches(false);
-			  dis2 = new DataInputStream(urlConn2.getInputStream());
-			  
-			  int j=0; 
-			  while ((s = dis2.readLine()) != null){
-				   stringArray2[j] = s;
-				   j++;
-			  }
-			  
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    }
-    
-  
-   
+     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       
+      
+        //sign in using facebook  
         
-        
-        facebook.authorize(this, new DialogListener() {
+       facebook.authorize(this, new DialogListener() {
             public void onComplete(Bundle values) {}
 
             public void onFacebookError(FacebookError error) {}
@@ -99,32 +41,26 @@ public class newMovieRecommendationActivity extends Activity {
 
             public void onCancel() {}
         });
-        
        
-        lv1=(ListView)findViewById(R.id.ListView01);
-        // By using setAdpater method in listview we an add string array in list.
+       
+       String[] buttons = getResources().getStringArray(R.array.main_buttons);
+       setListAdapter(new ArrayAdapter<String>(this, R.layout.list_view, buttons));
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          
+        	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        		MovieScreen.setListValue(id);
+        		Intent i = new Intent(newMovieRecommendationActivity.this, MovieScreen.class);
+        		startActivity(i);
+        	  
+             }
+        });
         
-         lv1.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , stringArray1));
-         lv1.setTextFilterEnabled(true);
     }
-
-   
-    public void buttonHandler(View view){
-    	
-    	String[] stringArray;
-    	
-    	if(view.getId() == R.id.upcomingMovieButton){
-    		stringArray = stringArray2;
-    	}
-    	else{
-    		stringArray = stringArray1;
-    	}
-    	
-    	
-
-    	  
-
-    }
+    
     
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
